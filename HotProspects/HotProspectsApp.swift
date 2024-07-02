349091908375d6043c7cac4183ab6bd5de5f9c07
Álvaro Gascón <144269155+gascondev@users.noreply.github.com>
2024-07-02 07:04:06 +0200

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Álvaro Gascón on 2/7/24.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
