787932b5329515739d327eeab47dfab937f14205
Álvaro Gascón <144269155+gascondev@users.noreply.github.com>
2024-07-02 07:24:58 +0200

    Letting users select items in a List

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -8,14 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+//    @State private var selection: String? // Para seleccionar uno
+    @State private var selection = Set<String>() // Para selección múltiple
+    
+    let users = ["Tohru", "Yuki", "Kyo", "Momiji"]
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        List(users, id: \.self, selection: $selection) { user in
+            Text(user)
         }
-        .padding()
+//        // Al seleccionar uno
+//        if let selection {
+//            Text("you selected \(selection)")
+//        }
+        if selection.isEmpty == false {
+            Text("You selected \(selection.formatted())")
+        }
+        EditButton()
+
     }
 }
 
