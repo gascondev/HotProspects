db8c7c535597d6f263b554c287cee5f541f83312
Álvaro Gascón <144269155+gascondev@users.noreply.github.com>
2024-07-05 20:37:04 +0200

    Building our tab bar

---
+++ HotProspects/MeView.swift
@@ -0,0 +1,18 @@
+//
+//  MeView.swift
+//  HotProspects
+//
+//  Created by Álvaro Gascón on 5/7/24.
+//
+
+import SwiftUI
+
+struct MeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MeView()
+}
---
+++ HotProspects/ProspectsView.swift
@@ -0,0 +1,40 @@
+//
+//  ProspectsView.swift
+//  HotProspects
+//
+//  Created by Álvaro Gascón on 5/7/24.
+//
+
+import SwiftUI
+
+struct ProspectsView: View {
+    
+    enum FilterType {
+        case none, contacted, uncontacted
+    }
+    
+    let filter: FilterType
+    
+    var title: String {
+        switch filter {
+        case .none:
+            "Everyone"
+        case .contacted:
+            "Contacted people"
+        case .uncontacted:
+            "Uncontacted people"
+        }
+    }
+    
+    
+    var body: some View {
+        NavigationStack {
+            Text("Hello, World!")
+                .navigationTitle(title)
+        }
+    }
+}
+
+#Preview {
+    ProspectsView(filter: .none)
+}
