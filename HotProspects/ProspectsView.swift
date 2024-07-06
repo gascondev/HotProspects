b5b1b1e8b9b718b2568007025c7059036e68f3b9
Álvaro Gascón <144269155+gascondev@users.noreply.github.com>
2024-07-06 21:13:45 +0200

    Dynamically filtering our SwiftData query

--- HotProspects/ProspectsView.swift
+++ HotProspects/ProspectsView.swift
@@ -29,17 +29,27 @@ struct ProspectsView: View {
         }
     }
     
+    init (filter: FilterType) {
+        self.filter = filter
+        
+        if filter != .none {
+            let showContactedOnly = filter == .contacted
+            
+            _prospects = Query(filter: #Predicate {
+                $0.isContacted == showContactedOnly
+            }, sort: [SortDescriptor(\Prospect.name)])
+        }
+    }
+    
     
     var body: some View {
-        NavigationStack {
-            Text("People: \(prospects.count)")
-                .navigationTitle(title)
-                .toolbar {
-                    Button("Scan", systemImage: "qrcode.viewfinder") {
-                        let prospect = Prospect(name: "Paul Hudson", emailAddress: "paul@hackingwithswift.com", isContacted: false)
-                        modelContext.insert(prospect)
-                    }
-                }
+        List(prospects) { prospect in
+            VStack(alignment: .leading) {
+                Text(prospect.name)
+                    .font(.headline)
+                Text(prospect.emailAddress)
+                    .foregroundStyle(.secondary)
+            }
         }
     }
 }
